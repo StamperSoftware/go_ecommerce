a9d9f448f8f79401dac26f9ebc75c349bf1f15be
StamperSoftware <StamperSoftware@gmail.com>
2024-07-14 12:20:40 -0400

    Initial site setup

---
+++ cmd/web/handlers.go
@@ -0,0 +1,9 @@
+﻿package main
+
+import "net/http"
+
+func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
+	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
+		app.errorLog.Println(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module ecommerce
+
+go 1.22.3
+
+require github.com/go-chi/chi/v5 v5.1.0 // indirect
